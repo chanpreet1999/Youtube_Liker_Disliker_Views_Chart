87907277dfe751913991825a43d104a010420306
Chanpreet Singh <chanpreet1999@gmail.com>
2020-06-17 20:18:07 +0530

    Parallely working for 10 videos

--- fair/activity/YoutubeLiker.js
+++ fair/activity/YoutubeLiker.js
@@ -3,14 +3,13 @@ const puppeteer = require('puppeteer');
 const { table } = require('console');
 let link = process.argv[2];
 let  allData = [];
-
+let href = [];
+let idx = 0;   
 //https://www.youtube.com/channel/UCOhHO2ICt0ti9KAh-QHvttQ
 //https://www.youtube.com/channel/UCRAmlxaK4Chru5gbTzxbHug
 
 (async function () {
     try {
-        let allPro = [];
-        let href = [];
         //start browser
         let browser = await puppeteer.launch({      //these are launch options
             headless: false,
@@ -24,35 +23,43 @@ let  allData = [];
         await tab.goto(`${link}/videos`, { waitUntil: "networkidle0" , timeout : 45 * 1000});
 
         const vidList = await tab.$$('#contents .style-scope.ytd-section-list-renderer a#video-title');
-
         for (let ele of vidList) {
             let curHref = await tab.evaluate(el => el.getAttribute("href"), ele);
             let curLink = 'https://www.youtube.com' + curHref;
             href.push(curLink);
         }
-
-        await handleSingleVid(browser, href[0]);
-        console.log('called resolve');
+       
+        while(idx < href.length)
+        {
+            await limitTabs( browser, idx, idx + 10 );
+            idx += 10;
+        }
+        console.table(allData);
         
-        console.log(allData);
-        
-        // let willHandleOneVid = await handleSingleVid(browser, tab, curLink);
-        // allPro.push(willHandleOneVid);
-        // let completeArr = await Promise.all(allPro);
-        // console.table(completeArr);
-
+               
     } catch (err) {
         console.log(err);
     }
 })();
 
-async function handleSingleVid(browser, link) {
+async function limitTabs(browser, start, end)
+{
+    let limitP  =[];
+    for(let i = start; i < href.length && i < end; i++)
+    {
+        let singlePage = handleSingleVid(browser, href[i], i);
+        limitP.push( singlePage );
+    }
+    await Promise.all(limitP);
+}
+
+async function handleSingleVid(browser, link, order) {
     return new Promise(async function (resolve, reject) {
         
         let tab = await browser.newPage()
         let obj = {};
-        
-        await tab.goto(link, { waitUntil: "networkidle0" });
+
+        await tab.goto(link, { waitUntil: "networkidle0" , timeout : 60 * 1000});
         
         await tab.waitForSelector('yt-formatted-string#text[aria-label]');
         const data = await tab.$$('yt-formatted-string#text[aria-label]');
@@ -60,7 +67,7 @@ async function handleSingleVid(browser, link) {
         obj.dislikes = await tab.evaluate(el => el.getAttribute('aria-label'), data[1]);
         let views = await tab.waitForSelector('.view-count.style-scope.yt-view-count-renderer');
         obj.views = await tab.evaluate(element => { return element.innerText }, views);
-        
+        obj.order = order;
         allData.push(obj);
         
         await tab.close();
